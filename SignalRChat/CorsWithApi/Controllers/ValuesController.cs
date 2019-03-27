670d7168a9790e1d6316f4d9d4f7d39f57592a99
adriannetubig <adriannetubig@gmail.com>
2019-03-27 09:25:41 +0800

    Removed not needed methods

--- SignalRChat/CorsWithApi/Controllers/ValuesController.cs
+++ SignalRChat/CorsWithApi/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CorsWithApi.Hubs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
@@ -28,31 +29,6 @@ namespace CorsWithApi.Controllers
             //await _hub.Clients.All.SendAsync("show_data", data);
 
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
